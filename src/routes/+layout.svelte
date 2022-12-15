3c9d6266d420d93f3633148a1e11274918692b43
Nick <nconkl1@outlook.com>
2022-12-15 17:52:50 -0500

    styled basic navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '$lib/app.css';
+	import logo from '$lib/assets/pokeball_logo.png';
 </script>
 
 <svelte:head>
@@ -7,13 +8,42 @@
 </svelte:head>
 
 <header>
-	<nav
-		class="bg-gray-700 drop-shadow-md px-6 mb-6 items-center align-middle justify-between flex h-14 gap-5"
-	>
-		<h1 class="text-2xl heading-title"><a href="/">Ultidex</a></h1>
-		<div class="space-x-5">
-			<a href="about">About</a>
-			<!-- TODO: Implement dark mode toggle-->
+	<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-red-600 mb-6">
+		<div class="container flex flex-wrap items-center justify-between mx-auto">
+			<a href="/" class="flex items-center">
+				<img src={logo} class="h-6 mr-4 sm:h-9" alt="Ultidex Logo" />
+				<span class="self-center text-2xl font-bold whitespace-nowrap">Ultidex</span>
+			</a>
+			<button
+				data-collapse-toggle="navbar-default"
+				type="button"
+				class="inline-flex items-center p-2 ml-3 text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200  dark:hover:bg-red-700 dark:focus:ring-red-800"
+				aria-controls="navbar-default"
+				aria-expanded="false"
+			>
+				<span class="sr-only">Open main menu</span>
+				<svg
+					class="w-6 h-6"
+					aria-hidden="true"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+						clip-rule="evenodd"
+					/></svg
+				>
+			</button>
+			<div class="hidden w-full md:block md:w-auto" id="navbar-default">
+				<ul
+					class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 "
+				>
+					<li>
+						<a href="/" class="block py-2 pl-3 pr-4 " aria-current="page">Home</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</nav>
 </header>
