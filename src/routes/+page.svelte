19fbfb538b0329e39f0734f4b12cedf7ccb19d9a
Nick <nconkl1@outlook.com>
2022-12-15 19:20:54 -0500

    created sample pill

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex">
+	<div
+		class="pokemon-type rounded-md py-1 shadow-md border-2 border-green-500 bg-green-700 align-middle justify-center w-auto"
+	>
+		<div class="px-3 text-center font-medium text-sm">Grass</div>
+	</div>
+</div>
