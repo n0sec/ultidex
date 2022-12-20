d68837562e4d702fa5c20b73e05ac456aebc696d
Nick <nconkl1@outlook.com>
2022-12-19 19:50:16 -0500

    created details container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,56 @@
 	import type { PageData } from './$types';
 	export let data: PageData;
 
-	console.log(data.promises);
+	let pokemons = data.pokemon_v2_pokemon;
 </script>
 
-<div class="flex">
+{#each pokemons as pokemon}
 	<div
-		class="pokemon-type rounded-md py-1 shadow-md border-2 border-green-500 bg-green-700 align-middle justify-center w-auto"
+		class="bg-gray-700 container mx-auto md:shadow-md md:w-1/3 flex align-middle flex-col p-3 mt-3 border-2 rounded-md border-gray-600"
 	>
-		<div class="px-3 text-center font-medium text-sm">Grass</div>
+		<h1 class="uppercase font-medium">
+			<span class="text-gray-400 mr-3">#{pokemon.id.toString().padStart(4, '0')}</span
+			>{pokemon.name}
+		</h1>
+		<div class="flex flex-row mt-6 justify-around">
+			{#each pokemon.pokemon_v2_pokemonsprites as sprites, index}
+				<img
+					src={JSON.parse(sprites.sprites)['front_default']}
+					alt="{pokemon.name} Sprite"
+					class="pokemon-sprite aspect-auto w-48 h-48"
+				/>
+			{/each}
+			<div class="flex-col my-auto">
+				{#each pokemon.pokemon_v2_pokemonstats as stat}
+					<div class="font-medium">
+						<span class="font-medium inline-block mr-6 uppercase"
+							>{stat.pokemon_v2_stat.name.replace('-', ' ')}</span
+						>
+						{stat.base_stat}
+					</div>
+				{/each}
+			</div>
+		</div>
+		<div class="flex flex-row space-x-3 mt-3 items-center">
+			{#each pokemon.pokemon_v2_pokemontypes as types}
+				<div class="flex">
+					<div
+						class="pokemon-type rounded-md py-1 shadow-md border-2 border-green-500 bg-green-700"
+					>
+						<div class="px-3 text-center font-medium text-sm">
+							{types.pokemon_v2_type.name.charAt(0).toUpperCase() +
+								types.pokemon_v2_type.name.slice(1)}
+						</div>
+					</div>
+				</div>
+			{/each}
+			<a href="/" class="mr-3 hover:text-gray-300">Details</a>
+		</div>
 	</div>
-</div>
+{/each}
+
+<style>
+	.pokemon-sprite {
+		image-rendering: pixelated;
+	}
+</style>
