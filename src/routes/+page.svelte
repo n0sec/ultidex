d35363565d3cb6aa061500a7b2c956bd03dbdad5
Nick <nconkl1@outlook.com>
2022-12-20 20:02:37 -0500

    updated home with components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
+	import PokemonStat from '$lib/components/PokemonStat.svelte';
+	import PokemonType from '$lib/components/PokemonType.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
 
 	let pokemons = data.pokemon_v2_pokemon;
+	console.log(pokemons);
 </script>
 
 {#each pokemons as pokemon}
 	<div
-		class="bg-gray-700 container mx-auto md:shadow-md md:w-1/3 flex align-middle flex-col p-3 mt-3 border-2 rounded-md border-gray-600"
+		class="bg-gray-700 container md:mx-auto md:shadow-md md:w-[35%] flex align-middle flex-col p-3 mt-3 border-2 rounded-md border-gray-600"
 	>
-		<h1 class="uppercase font-medium">
+		<h1 class="uppercase font-semibold">
 			<span class="text-gray-400 mr-3">#{pokemon.id.toString().padStart(4, '0')}</span
 			>{pokemon.name}
 		</h1>
-		<div class="flex flex-row mt-6 justify-around">
+		<div class="flex flex-row mt-6">
 			{#each pokemon.pokemon_v2_pokemonsprites as sprites}
 				<img
 					src={JSON.parse(sprites.sprites)['front_default']}
@@ -21,29 +24,23 @@
 					class="pokemon-sprite aspect-auto w-48 h-48"
 				/>
 			{/each}
-			<div class="flex-col my-auto">
+			<div class="flex-col my-auto w-full">
 				{#each pokemon.pokemon_v2_pokemonstats as stat}
-					<div class="font-medium">
-						<span class="font-medium inline-block mr-6 uppercase"
-							>{stat.pokemon_v2_stat.name.replace('-', ' ')}</span
-						>
-						{stat.base_stat}
-					</div>
+					<PokemonStat
+						statName={stat.pokemon_v2_stat.name}
+						baseStat={stat.base_stat}
+						bind:statTotal={pokemon.pokemon_v2_pokemonstats_aggregate.aggregate.sum.base_stat}
+					/>
 				{/each}
+				<!-- ? Can we set this equal to a variable somehow? -->
+				<div class="font-bold pl-4 pt-5">
+					Total: {pokemon.pokemon_v2_pokemonstats_aggregate.aggregate.sum.base_stat}
+				</div>
 			</div>
 		</div>
 		<div class="flex flex-row space-x-3 mt-3 items-center">
 			{#each pokemon.pokemon_v2_pokemontypes as types}
-				<div class="flex">
-					<div
-						class="pokemon-type rounded-md py-1 shadow-md border-2 border-green-500 bg-green-700"
-					>
-						<div class="px-3 text-center font-medium text-sm">
-							{types.pokemon_v2_type.name.charAt(0).toUpperCase() +
-								types.pokemon_v2_type.name.slice(1)}
-						</div>
-					</div>
-				</div>
+				<PokemonType pokemonType={types.pokemon_v2_type.name} />
 			{/each}
 			<a href="/" class="mr-3 hover:text-gray-300">Details</a>
 		</div>
