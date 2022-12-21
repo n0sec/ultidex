34de356e15a6068304f3909a840aa2100e7a0e83
Nick <nconkl1@outlook.com>
2022-12-21 13:31:18 -0500

    added scroll to top button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,13 @@
 	import '$lib/app.css';
 	import logo from '$lib/assets/pokeball_logo.png';
 	import { page } from '$app/stores';
+	import { browser } from '$app/environment';
+
+	function toTop() {
+		document.body.scrollIntoView({
+			behavior: 'smooth'
+		});
+	}
 </script>
 
 <svelte:head>
@@ -72,6 +79,19 @@
 </header>
 
 <main>
+	<button
+		class="fixed bottom-10 right-10 rounded-full bg-gradient-to-t from-red-600 to-red-700 h-12 w-12"
+		type="button"
+		on:click={toTop}
+		><svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 16 16"
+			class="h-6 w-full text-center fill-neutral-800"
+			><path
+				d="M3.47 7.78a.75.75 0 010-1.06l4.25-4.25a.75.75 0 011.06 0l4.25 4.25a.751.751 0 01-.018 1.042.751.751 0 01-1.042.018L9 4.81v7.44a.75.75 0 01-1.5 0V4.81L4.53 7.78a.75.75 0 01-1.06 0z"
+			/></svg
+		></button
+	>
 	<slot />
 </main>
 
