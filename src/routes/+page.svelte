a874fc01ce1f028f582bf9190ed05ffb2493189b
Nick <nconkl1@outlook.com>
2022-12-21 17:58:29 -0500

    added search box

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,43 @@
 	export let data: PageData;
 
 	let pokemons = data.pokemon_v2_pokemon;
+	let searchInput: string;
 	console.log(pokemons);
+
+	function updateQueryParams(event: any) {
+		window.history.replaceState(null, '', `?q=${searchInput}`);
+	}
 </script>
+
+<div class="container md:mx-auto md:w-[35%] mb-6">
+	<div class="relative">
+		<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+			<svg
+				aria-hidden="true"
+				class="w-5 h-5 text-gray-500 dark:text-gray-400"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+				/></svg
+			>
+		</div>
+		<input
+			type="search"
+			id="default-search"
+			bind:value={searchInput}
+			class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-red-500 focus:border-red-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
+			placeholder="Search by name, ID or type"
+			on:input={updateQueryParams}
+		/>
+	</div>
+	<hr class="my-3 h-px bg-gray-200 border-0 dark:bg-gray-700" />
+</div>
 
 {#each pokemons as pokemon}
 	<div
