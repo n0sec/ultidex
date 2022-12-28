df9f60a5b0239fbfeb71dd001562ec2aef350b1d
Nick <nconkl1@outlook.com>
2022-12-28 15:58:55 -0500

    added outer div

--- src/lib/components/PokemonDetails.svelte
+++ src/lib/components/PokemonDetails.svelte
@@ -13,66 +13,66 @@
 	console.log(ev_yield);
 </script>
 
-<table class="table-auto">
-	<tr>
-		<th>
-			<p>ID</p>
-		</th>
-		<td>
-			{id}
-		</td>
-	</tr>
-	<tr>
-		<th>
-			<p>Height</p>
-		</th>
-		<td>
-			{height} m
-		</td>
-	</tr>
+<div>
+	<table class="table-auto">
+		<tr>
+			<th>
+				<p>ID</p>
+			</th>
+			<td>
+				{id}
+			</td>
+		</tr>
+		<tr>
+			<th>
+				<p>Height</p>
+			</th>
+			<td>
+				{height} m
+			</td>
+		</tr>
 
-	<tr>
-		<th>
-			<p>Weight</p>
-		</th>
-		<td>
-			{weight} kg
-		</td>
-	</tr>
+		<tr>
+			<th>
+				<p>Weight</p>
+			</th>
+			<td>
+				{weight} kg
+			</td>
+		</tr>
 
-	<tr>
-		<th> Type </th>
-		<td>
-			<div class="flex flex-row mt-3 items-center justify-between">
-				<div class="flex space-x-3">
-					{#each type as pokemonType}
-						<PokemonType pokemonType={pokemonType.pokemon_v2_type.name} />
+		<tr>
+			<th> Type </th>
+			<td>
+				<div class="flex flex-row mt-3 items-center justify-between">
+					<div class="flex space-x-3">
+						{#each type as pokemonType}
+							<PokemonType pokemonType={pokemonType.pokemon_v2_type.name} />
+						{/each}
+					</div>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<th> Abilities </th>
+			<td>
+				<div>
+					{#each abilities as ability}
+						<p>{ability.pokemon_v2_ability.name}</p>
 					{/each}
 				</div>
-			</div>
-		</td>
-	</tr>
-
-	<tr>
-		<th> Abilities </th>
-		<td>
-			<div>
-				{#each abilities as ability}
-					<p>{ability.pokemon_v2_ability.name}</p>
-				{/each}
-			</div>
-		</td>
-	</tr>
-
-	<tr>
-		<th> EV Yield </th>
-		<td>
-			<div>
-				<p>{ev_yield[0].effort} {ev_yield[0].pokemon_v2_stat.name.replace('-', ' ')}</p>
-			</div>
-		</td>
-	</tr>
-</table>
+			</td>
+		</tr>
+		<tr>
+			<th> EV Yield </th>
+			<td>
+				<div>
+					<p>{ev_yield[0].effort} &nbsp; {ev_yield[0].pokemon_v2_stat.name.replace('-', ' ')}</p>
+				</div>
+			</td>
+		</tr>
+	</table>
+</div>
 
 <style>
 	p {
