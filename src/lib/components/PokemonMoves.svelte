9bcd84de23394b9c239f8b1c1f4a4f1ea68485ba
Nick <nconkl1@outlook.com>
2022-12-28 19:15:22 -0500

    wip - moves table

---
+++ src/lib/components/PokemonMoves.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let moves: Array<any>;
+
+    let levelUpMoves = moves.
+</script>
+
+<table class="border-collapse border border-slate-500 w-full">
+	<thead>
+		<tr>
+			<th class="border border-slate-600">Level</th>
+			<th class="border border-slate-600">Move Name</th>
+			<th class="border border-slate-600">Type</th>
+			<th class="border border-slate-600">Power</th>
+			<th class="border border-slate-600">Accuracy</th>
+			<th class="border border-slate-600">PP</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="border border-slate-700">Indiana</td>
+			<td class="border border-slate-700">Indianapolis</td>
+		</tr>
+		<tr>
+			<td class="border border-slate-700">Ohio</td>
+			<td class="border border-slate-700">Columbus</td>
+		</tr>
+		<tr>
+			<td class="border border-slate-700">Michigan</td>
+			<td class="border border-slate-700">Detroit</td>
+		</tr>
+	</tbody>
+</table>
