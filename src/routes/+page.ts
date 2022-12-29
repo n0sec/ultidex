a47ad3e25793c19abcdf7670f352f263e039d3db
Nick <nconkl1@outlook.com>
2022-12-29 15:30:04 -0500

    added statTotal to page.ts

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,14 +1,23 @@
 import type { PageLoad } from './$types';
 
 export const load = (async ({ fetch }) => {
-	const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20`);
+	const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=2?offset=0`);
 	const data = await response.json();
 
-	const pokemonData = await Promise.all(
+	let pokemonData = await Promise.all(
 		data.results.map((result: { url: RequestInfo | URL }) =>
 			fetch(result.url).then((response) => response.json())
 		)
 	);
+
+	pokemonData = pokemonData.map((v) => {
+		let statTotal: number = 0;
+		for (let stat of v.stats) {
+			statTotal += stat.base_stat;
+		}
+		return { ...v, statTotal };
+	});
+
 	return {
 		pokemonData
 	};
