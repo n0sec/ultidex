5e0c2b0938d434ef7bc6a56ae3c72eb4d2a09259
Nick <nconkl1@outlook.com>
2022-12-30 13:12:47 -0500

    added pagination controls

---
+++ src/lib/components/PaginationBack.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageData } from '../../routes/$types';
+	import PaginationBackIcon from './PaginationBackIcon.svelte';
+
+	export let data: PageData;
+</script>
+
+<div class="flex items-center align-middle">
+	<a
+		href="?offset={data.offset - 20}"
+		class="bg-gradient-to-b from-gray-700 to-gray-800 border border-gray-600 focus:outline-none focus:ring-1 focus:ring-slate-600 ring-offset-1 shadow-inner px-4 py-2.5 rounded-md text-sm"
+		><PaginationBackIcon class="w-5 h-5" /></a
+	>
+</div>
