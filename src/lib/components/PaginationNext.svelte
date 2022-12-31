304399636afe8ee563ef9ab01e5c51bc8824da1e
Nick Conklin <nconkl1@outlook.com>
2022-12-31 09:39:45 -0500

    removed href in anchor tags for back and next

--- src/lib/components/PaginationNext.svelte
+++ src/lib/components/PaginationNext.svelte
@@ -7,7 +7,7 @@
 
 <div class="flex items-center align-middle">
 	<a
-		href="/?offset={data.offset + 20}"
+		href=""
 		class="bg-gradient-to-b from-gray-700 to-gray-800 border border-gray-600 focus:outline-none focus:ring-1 focus:ring-slate-600 ring-offset-1 shadow-inner px-4 py-2.5 rounded-md text-sm"
 		><PaginationNextIcon class="w-5 h-5" /></a
 	>
