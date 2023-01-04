02a5b43b2d8511b956ed4dd50a257f11720b29a8
Nick Conklin <nconkl1@outlook.com>
2023-01-04 12:38:48 -0500

    added offset href to next and back buttons

--- src/lib/components/PaginationBack.svelte
+++ src/lib/components/PaginationBack.svelte
@@ -7,7 +7,7 @@
 
 <div class="flex items-center align-middle">
 	<a
-		href=""
+		href="/?offset={data.offset - 20}"
 		class="bg-gradient-to-b from-gray-700 to-gray-800 border border-gray-600 focus:outline-none focus:ring-1 focus:ring-slate-600 ring-offset-1 shadow-inner px-4 py-2.5 rounded-md text-sm"
 		><PaginationBackIcon class="w-5 h-5" /></a
 	>
