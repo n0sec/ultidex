7e5d80bd34202bc67d3ef127eb5dee6c498ad4aa
Nick Conklin <nconkl1@outlook.com>
2023-01-06 14:03:23 -0500

    changed to inline-grid

--- src/lib/components/PaginationNext.svelte
+++ src/lib/components/PaginationNext.svelte
@@ -5,7 +5,7 @@
 	export let data: PageData;
 </script>
 
-<div class="flex items-center align-middle">
+<div class="inline-grid items-center align-middle">
 	<a
 		href="/?page={data.pageNumber + 1}"
 		class="bg-gradient-to-b from-gray-700 to-gray-800 border border-gray-600 focus:outline-none focus:ring-1 focus:ring-slate-600 ring-offset-1 shadow-inner px-4 py-2.5 rounded-md text-sm"
